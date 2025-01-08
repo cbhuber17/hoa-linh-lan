ae402590c5cf96c6b2aa7cfcd5e8fc21f16ce714
Colin Huber <cbhuber@gmail.com>
2025-01-08 16:51:19 -0700

    fix: Path to flower images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,62 +1,62 @@
 export const flowerImages = [
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa11.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa01.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa02.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa02.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa03.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa03.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa04.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa04.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa05.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa05.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa06.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa06.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa07.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa07.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa08.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa08.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa09.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa09.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa10.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa10.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa11.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa11.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa12.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa12.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa13.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa13.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa14.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa14.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa15.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa15.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa16.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa16.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa17.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa17.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa18.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa18.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa19.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa19.png",
   },
   {
-    fileName: "/hoa-linh-lan/public/flowers/Hoa20.png",
+    fileName: "/hoa-linh-lan/flowers/Hoa20.png",
   },
 ];
