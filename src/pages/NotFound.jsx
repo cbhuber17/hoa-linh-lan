5d7667782c24b709f767a242a6d8ead48ff5e009
Colin Huber <cbhuber@gmail.com>
2025-01-11 09:29:19 -0700

    fix: Not found page in Vietnamese

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,23 +7,20 @@ const NotFound = () => {
       <div className="not-found__content">
         <h1 className="not-found__content__title">404</h1>
         <p className="not-found__content__subtitle">
-          Apologies for the inconvenience.
+          Xin lỗi vì sự bất tiện này.
         </p>
         <p className="not-found__content__description">
-          It seems you’ve encountered a 404 error, and our enchanting flowers
-          are temporarily out of reach. We’re already working on restoring our
-          website so you can once again enjoy their beauty. Thank you for your
-          patience and understanding.
+          Có vẻ như bạn đã gặp phải lỗi 404 và những bông hoa mê hoặc của chúng
+          tôi tạm thời nằm ngoài tầm với. Chúng tôi đang nỗ lực khôi phục trang
+          web để bạn có thể một lần nữa thưởng thức vẻ đẹp của chúng. Cảm ơn bạn
+          đã kiên nhẫn và hiểu biết.
         </p>
         <a href="/">
-          <Button message="Back to the homepage" />
+          <Button message="Quay lại trang chủ" />
         </a>
       </div>
       <div className="not-found__image">
-        <img
-          src="/hoa-linh-lan//src/assets/Hero3.png"
-          alt="Bouquet of flowers"
-        />
+        <img src="/hoa-linh-lan//Hero1.png" alt="Bouquet of flowers" />
       </div>
     </section>
   );
